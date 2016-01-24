f7bdcaa6e07b7dc048afceadc0cbc56f805f7d3c
sgrinich <sgrinich@gmail.com>
2016-01-24 14:12:15 -0600

    Added filter, addentry buttons and a new swift view controller file for the news feed

--- ParseDemo/TeamSignUpViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TeamSignUpViewController.swift
-//  Caregiving
-//
-//  Created by Camille Benson on 1/13/16.
-//  Copyright © 2016 abearablecode. All rights reserved.
-//
-
-import Foundation
