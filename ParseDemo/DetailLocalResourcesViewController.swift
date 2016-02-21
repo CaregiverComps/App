dd26c78ae4f52c122f4607729b3f4cd2ad3be29c
brodie20j <brodiej@carleton.edu>
2016-02-20 20:10:29 -0600

    commit to pull

--- ParseDemo/DetailLocalResourcesViewController.swift
+++ ParseDemo/DetailLocalResourcesViewController.swift
@@ -9,31 +9,61 @@
 import UIKit
 
 class DetailLocalResourcesViewController: UITableViewController {
+    
     @IBOutlet weak var detailTitle: UINavigationItem!
+    @IBOutlet weak var resourceImageView: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var phoneTableViewCell: UITableViewCell!
+    @IBOutlet weak var websiteTableViewCell: UITableViewCell!
+    @IBOutlet weak var addressTableViewCell: UITableViewCell!
+    
     var passedValue:String?
     
-    @IBOutlet weak var descriptionTextView: UITextView!
-    @IBOutlet weak var descriptionTableViewCell: UITableViewCell!
-    var resourceDescription = "This is where the resource description goes. This is where the resource description goes. This is where the resource description goes. This is where the resource description goes. This is where the resource description goes. This is where the resource description goes. This is where the resource description goes. This is where the resource description goes. This is where the resource description goes. "
+    var tempDescription = "Replace the description. Replace the description. Replace the description. Replace the description. Replace the description. Replace the description. Replace the description. Replace the description. Replace the description. Replace the description. "
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set the cells with appropriate information
         self.detailTitle.title = self.passedValue
-//        self.descriptionTextView.text = self.resourceDescription
+        self.resourceImageView.image = UIImage(named: "user")
+        self.descriptionTextView.text = self.tempDescription
         
     }
     
+    /**
+        Passing string from last view controller
+     
+     */
     func passedValue(passed: String) {
         self.passedValue = passed
     }
     
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 6
-//        
-//    }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // Set cell selected as currentCell
+        let currentCell = tableView.cellForRowAtIndexPath(indexPath)! as UITableViewCell
+        
+        if(currentCell == phoneTableViewCell){
+            print("selected phone number")
+            // action to call number with phone
+        } else if (currentCell == websiteTableViewCell){
+            print("selected website URL")
+            // action to go use safari with this url
+        } else if (currentCell == addressTableViewCell) {
+            print("selected address")
+            //action to go to maps
+        }
+        
+        
+    }
     
     
+    @IBAction func shareResourceAction(sender: AnyObject) {
+        //Press 'action' button to share (as a contact?)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -42,18 +72,7 @@ class DetailLocalResourcesViewController: UITableViewController {
     
     
     
-    
-    //    override func viewWillAppear(animated: Bool) {
-    //        if (PFUser.currentUser() == nil) {
-    //            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-    //
-    //                let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Login")
-    //                self.presentViewController(viewController, animated: true, completion: nil)
-    //            })
-    //        }
-    //    }
-    
- 
+
     
 }
 
