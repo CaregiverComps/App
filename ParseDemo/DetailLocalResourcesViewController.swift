fdb6519165f950f68e310d38f6a1992ee269d0cc
Camille <camille@carlwireless-184-198.dyn.carleton.edu>
2016-02-21 19:39:27 -0600

    added contact name and email to local resources detail

--- ParseDemo/DetailLocalResourcesViewController.swift
+++ ParseDemo/DetailLocalResourcesViewController.swift
@@ -13,6 +13,8 @@ class DetailLocalResourcesViewController: UITableViewController {
     @IBOutlet weak var detailTitle: UINavigationItem!
     @IBOutlet weak var resourceImageView: UIImageView!
     @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var contactNameTableViewCell: UITableViewCell!
+    @IBOutlet weak var emailTableViewCell: UITableViewCell!
     @IBOutlet weak var phoneTableViewCell: UITableViewCell!
     @IBOutlet weak var websiteTableViewCell: UITableViewCell!
     @IBOutlet weak var addressTableViewCell: UITableViewCell!
@@ -28,9 +30,12 @@ class DetailLocalResourcesViewController: UITableViewController {
         self.detailTitle.title = self.passedValue?.resourceName;
         self.resourceImageView.image = UIImage(named: (self.passedValue?.imageName)!);
         self.descriptionTextView.text = self.passedValue?.body;
+        self.contactNameTableViewCell.textLabel?.text = self.passedValue?.contactName
         self.phoneTableViewCell.textLabel?.text=self.passedValue?.phoneNumber
+        self.emailTableViewCell.textLabel?.text = self.passedValue?.email
         self.websiteTableViewCell.textLabel?.text=self.passedValue?.website
         self.addressTableViewCell.textLabel?.text=self.passedValue?.address
+        
     }
     
     
@@ -42,20 +47,33 @@ class DetailLocalResourcesViewController: UITableViewController {
         // Set description size
         if(indexPath.section == 0) { return 146 }
         
-        // Hide phone number cell if needed
+        // Hide contact name cell if needed
         if(indexPath.section == 1 && indexPath.item == 0) {
+            if(self.passedValue?.contactName == "") {
+                return 0
+            }
+        }
+        
+        // Hide phone number cell if needed
+        if(indexPath.section == 1 && indexPath.item == 1) {
             if(self.passedValue?.phoneNumber == "") {
                 return 0
             }
         }
+        // Hide email cell if needed
+        if(indexPath.section == 1 && indexPath.item == 2) {
+            if(self.passedValue?.email == "") {
+                return 0
+            }
+        }
         // Hide website cell if needed
-        if(indexPath.section == 1 && indexPath.item == 1) {
+        if(indexPath.section == 1 && indexPath.item == 3) {
             if(self.passedValue?.website == "") {
                 return 0
             }
         }
         // Hide address cell if needed
-        if(indexPath.section == 1 && indexPath.item == 2) {
+        if(indexPath.section == 1 && indexPath.item == 4) {
             if(self.passedValue?.address == "") {
                 return 0
             }
@@ -70,10 +88,16 @@ class DetailLocalResourcesViewController: UITableViewController {
         let currentCell = tableView.cellForRowAtIndexPath(indexPath)! as UITableViewCell
         
         if(currentCell == phoneTableViewCell){
-            
+
             let phoneNumber = phoneTableViewCell.textLabel!.text!
             // action to call number with phone
             UIApplication.sharedApplication().openURL(NSURL(string: "tel://"+"\(phoneNumber)")!)
+            
+        } else if (currentCell == emailTableViewCell){
+            
+            let email = emailTableViewCell.textLabel!.text!
+            // action to go email contact
+            UIApplication.sharedApplication().openURL(NSURL(string: "mailto:\(email)")!)
             
         } else if (currentCell == websiteTableViewCell){
             
